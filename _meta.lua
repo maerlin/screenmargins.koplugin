933b114932f832c7dc06a3986c9b5d80cb522015
Pietro Baiguini <hello@pietro.info>
2026-06-13 16:50:59 +0200

    Forgot the v0.1.0 metadata

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,6 @@ local _ = require("gettext")
 return {
     name = "screenmargins",
     fullname = _("Screen margins"),
+    version = "0.1.0",
     description = _("Configure screen margins/viewport to handle devices with bezels that cover part of the screen."),
 }
